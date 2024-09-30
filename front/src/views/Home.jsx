859eac85c457c0dc04d5bfded41826bf72d4d1ca
ARagustinaramos <agusramos12@gmail.com>
2024-09-30 10:59:37 -0300

    agregando componente Cards

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import Footer from '../components/footer/Footer'
-import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer';
+import Header from '../components/header/Header';
+import Card from "../components/cards/Cards";
 import StickyNavbar from "../components/navbar/StickyNavbar";
 
 const Home = () => {
+  const cards = Array(5).fill(null); 
 
-    return (
-        <>
-            <StickyNavbar />
-            <Header />  
-            <Footer />
-        </> 
-    )
-
-}
+  return (
+    <>
+      <StickyNavbar />
+      <Header />
+      <div className="flex flex-col items-center space-y-8 mt-8">
+        {cards.map((_, index) => (
+          <Card key={index} />
+        ))}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
