6246ecafa6db39509b5a0d537e4696dcb31a3c09
ARagustinaramos <agusramos12@gmail.com>
2024-10-02 11:47:23 -0300

    Agrego Redireccionamiento de Card a Detail

--- front/src/components/cards/Cards.jsx
+++ front/src/components/cards/Cards.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 
 export default function Card() {
   return (
@@ -10,14 +12,12 @@ export default function Card() {
       <div className="p-6 ">
         <div>
           <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">Product</span>
-          <a
-            href="#"
-            className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
-            tabIndex="0"
-            role="link"
-          >
-            I Built A Successful Blog In One Year
-          </a>
+          <Link
+              to="/detail"
+              className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
+                 >
+              I Built A Successful Blog In One Year
+          </Link>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel.
             Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada
