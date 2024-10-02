26bb473bde543953aa3ba5f50acec066f2511dc0
ARagustinaramos <agusramos12@gmail.com>
2024-10-02 11:43:01 -0300

    Agrego vista Detail

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
-import Home from '../src/views/Home'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './views/home/Home';
+import Detail from './views/detail/Detail';
+
 function App() {
   return (
-    <>
-      <Home />
-    </>
-  )
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/detail" element={<Detail />} />  
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- front/src/views/home/Home.jsx
+++ front/src/views/home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Footer from '../components/footer/Footer';
-import Header from '../components/header/Header';
-import Card from "../components/cards/Cards";
-import QueryForm from "../components/queryform/QueryForm";
-import StickyNavbar from "../components/navbar/StickyNavbar";
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import Card from "../../components/cards/Cards";
+import QueryForm from "../../components/queryform/QueryForm";
+import StickyNavbar from "../../components/navbar/StickyNavbar";
 
 const Home = () => {
     const cards = Array(5).fill(null);
