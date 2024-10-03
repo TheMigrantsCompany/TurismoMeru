61eb7f1a718a918c0c539ed9f56a735b9379793a
ARagustinaramos <agusramos12@gmail.com>
2024-10-03 11:12:07 -0300

    Agrego Carrito de compras

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/home/Home';
 import Detail from './views/detail/Detail';
+import ShoppingCart from './views/shopping-cart/ShoppingCart';
 
 function App() {
   return (
     <Router> 
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/detail" element={<Detail />} />  
+        <Route path="/detail" element={<Detail />} /> 
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
       </Routes>
     </Router>
   );
