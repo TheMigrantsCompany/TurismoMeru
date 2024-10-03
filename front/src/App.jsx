872cd045bc7c513be23a8d2c483c36c8d13b698c
Iara Baudino <iaba.sur@gmail.com>
2024-10-03 10:51:49 -0300

    starting admin dashboard

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/home/Home';
 import Detail from './views/detail/Detail';
+import { DashboardAdmin } from './views/dashboardAdmin/DashboardAdmin';
 
 function App() {
   return (
     <Router> 
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/detail" element={<Detail />} />  
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/admin" element={<DashboardAdmin />} />
       </Routes>
     </Router>
   );
