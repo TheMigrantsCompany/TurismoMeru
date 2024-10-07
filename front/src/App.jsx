77cb13f0e4560db8bb130328165804e4b39166fc
ARagustinaramos <agusramos12@gmail.com>
2024-10-07 10:49:10 -0300

    Agrego Formulario de Perfil de usuario

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,6 +6,8 @@ import { DashboardAdmin } from './views/dashboardAdmin/DashboardAdmin';
 import { DashboardUser } from './views/dashboardUser/DashboarUser';
 import ShoppingCart from './views/shopping-cart/ShoppingCart';
 import StickyNavbar from './components/navbar/StickyNavbar';
+import ProfileForm from './views/dashboardUser/ProfileForm';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path="/admin" element={<DashboardAdmin />} />
         <Route path="/user" element={< DashboardUser />} />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/profile" element={<ProfileForm />} />
       </Routes>
     </Router>
   );
