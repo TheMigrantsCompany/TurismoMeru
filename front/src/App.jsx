82bb50ec376176b26ff50832734d59c877462096
Iara Baudino <iaba.sur@gmail.com>
2024-10-08 19:26:40 -0300

    inicio dashboard admin - gestion de reservas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,16 +6,22 @@ import { DashboardAdmin } from './views/dashboardAdmin/DashboardAdmin';
 import { DashboardUser } from './views/dashboardUser/DashboarUser';
 import ShoppingCart from './views/shopping-cart/ShoppingCart';
 import StickyNavbar from './components/navbar/StickyNavbar';
+import { ReservationManagement } from './views/dashboardAdmin/dashAdminViews/ReservationManagement';
 
 function App() {
   return (
     <Router> 
       < StickyNavbar />
       <Routes>
-        <Route path="/" element={<Home />} /> 
+      <Route path="/" element={<Home />} /> 
         <Route path="/detail" element={<Detail />} />
-        <Route path="/admin" element={<DashboardAdmin />} />
-        <Route path="/user" element={< DashboardUser />} />
+         {/* Ruta para la sección de administración */}
+         <Route path="/admin" element={<DashboardAdmin />}>
+          {/* Subrutas dentro de la administración */}
+          <Route path="reservas" element={<ReservationManagement />} />
+          {/* Puedes añadir otras subrutas aquí */}
+        </Route>
+        <Route path="/user" element={<DashboardUser />} />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
       </Routes>
     </Router>
