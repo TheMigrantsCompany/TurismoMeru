4125d57598a269287b6d3f7377a0ae335de7af90
Iara Baudino <iaba.sur@gmail.com>
2024-10-09 11:58:19 -0300

    View gestion de reservas OK

--- front/src/views/dashboardAdmin/dashAdminViews/ReservationManagement.jsx
+++ front/src/views/dashboardAdmin/dashAdminViews/ReservationManagement.jsx
@@ -1,20 +1,24 @@
 //vista para todo lo que corresponda a la gestion de reservas.
-// INPUT DE BUSQUEDA - TABLA DE RESERVAS - MODAL  PARA VER Y MODIFICAR RESERVAS - SWEETALERT PARA de éxito o error al realizar acciones (modificar, eliminar, confirmar reservas). 
-
-import React from 'react';
-import SearchInput from '../../../components/inputs/SearchInput'; 
-
+// INPUT DE BUSQUEDA - TABLA DE RESERVAS - MODAL  PARA VER Y MODIFICAR RESERVAS - SWEETALERT PARA de éxito o error al realizar acciones (modificar, eliminar, confirmar reservas).
+import React from "react";
+import SearchInput from "../../../components/inputs/SearchInput";
+import { ReservationsTable } from "../../../components/tables/admin/Reservations";
 export function ReservationManagement() {
   const handleSearch = (query) => {
-    // Aquí iría la función de búsqueda
+    // Aquí iría la función de búsqueda
     console.log('Buscar reservas con:', query);
   };
 
   return (
     <div>
       <h1>Gestión de Reservas</h1>
+      {/* Input de búsqueda */}
       <SearchInput onSearch={handleSearch} />
-      {/* Aquí iría el resto de tu contenido, como la tabla de reservas */}
+      
+      {/* Aquí se renderiza la tabla de reservas */}
+      <ReservationsTable />
+
+      {/* Aquí podrías agregar el modal y otros componentes adicionales */}
     </div>
   );
 }
