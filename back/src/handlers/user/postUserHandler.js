bd77dfcb4e2687cebd67c50c85d2d89a51be3547
Laura Camila Tiusabá Gómez <108424409+LauraCamila99@users.noreply.github.com>
2024-10-30 10:14:49 -0500

    cambios parciales firebase

--- back/src/handlers/user/postUserHandler.js
+++ back/src/handlers/user/postUserHandler.js
@@ -1,9 +1,14 @@
 const postUserController = require('../../controllers/user/postUserController');
+const verifyToken = require('../../middlewares/verifyToken');
 
 const postUserHandler = async (req, res) => {
   try {
-    const newUser = await postUserController(req.body);
-    res.status(201).json(newUser);
+    // Llama al middleware `verifyToken` para autenticar el token
+    await verifyToken(req, res, async () => {
+      // Luego, llama al controlador para crear el usuario
+      const newUser = await postUserController(req.body);
+      res.status(201).json(newUser);
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
