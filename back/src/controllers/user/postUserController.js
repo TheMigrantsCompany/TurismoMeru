837f39468f0ffdf447aa46b2f30676ce436421c2
Iara Baudino <iaba.sur@gmail.com>
2024-11-22 16:19:38 -0300

    roles asignados en backend para admin y customer

--- back/src/controllers/user/postUserController.js
+++ back/src/controllers/user/postUserController.js
@@ -9,6 +9,20 @@ const postUserController = async (userData) => {
     throw new Error('El usuario con ese correo ya existe');
   }
 
+  // Lista de correos que deben tener el rol de admin
+  const adminEmails = [
+    'franciscomeruviajes@gmail.com',
+    'lautiu9@gmail.com',
+    'agusramos12@gmail.com',
+    'iaba.sur@gmail.com',
+  ];
+
+  // Determinar el rol del usuario
+  const role = adminEmails.includes(email) ? 'admin' : 'customer';
+  
+  // Asignar el rol al userData
+  userData.role = role;
+
   // Creación del nuevo usuario
   const newUser = await User.create(userData);
   return newUser;
