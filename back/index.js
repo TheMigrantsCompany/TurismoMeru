300af0ce2dbe1bd90f3bd63a935c86fc237335c5
Iara Baudino <iaba.sur@gmail.com>
2025-02-22 18:55:10 -0300

    Fix: update port configuration for production

--- back/index.js
+++ back/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const server = require("./src/server");
 const { sequelize } = require("./src/config/db");
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3001; // Usar el puerto que asigna Render en producción
 
 server.listen(PORT, async () => {
-	try {
-		await sequelize.sync({  force:false });
-		console.log(`Server listening on port http://localhost:${PORT}`);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    await sequelize.sync({ force: false });
+    console.log(`Server listening on port ${PORT}`);
+  } catch (error) {
+    console.error(error);
+  }
 });
