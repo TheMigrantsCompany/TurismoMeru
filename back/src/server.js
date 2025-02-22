d07e38e9d9d1ed9123694b6b61f01b3d1124fa24
Iara Baudino <iaba.sur@gmail.com>
2025-02-22 19:05:47 -0300

    Fix: update CORS configuration for production

--- back/src/server.js
+++ back/src/server.js
@@ -23,11 +23,11 @@ server.use(
   "*",
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://bearing-settled-consult-je.trycloudflare.com", // tunel para back cloudflared tunnel --url http://localhost:3001
-      "https://self-brad-cz-previously.trycloudflare.com", //  tunel para front cloudflared tunnel --url http://localhost:5173
+      "http://localhost:5173", // desarrollo
+      "https://www.meruviajes.tur.ar", // producción
+      "https://meruviajes.tur.ar", // producción sin www
     ],
-    credentials: true, // Permite cookies y autenticación
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
 );
