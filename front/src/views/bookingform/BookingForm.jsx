bd470b61190269cc8b27e33c30e93ef0e7cca54e
Agustina Ramos <144278185+ARagustinaramos@users.noreply.github.com>
2025-02-26 19:39:37 -0300

    Update BookingForm.jsx

--- front/src/views/bookingform/BookingForm.jsx
+++ front/src/views/bookingform/BookingForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import AuthContext from "../context/AuthContext";
+import { AuthContext } from "../../firebase/AuthContext";
 
 const BookingForm = ({ serviceOrder }) => {
   const navigate = useNavigate();
