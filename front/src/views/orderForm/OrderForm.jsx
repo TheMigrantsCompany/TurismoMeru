a3b6e17bcdcf0639384f0276ad895490cb6db332
Agustina Ramos <144278185+ARagustinaramos@users.noreply.github.com>
2025-03-05 12:04:28 -0300

    Update OrderForm.jsx

--- front/src/views/orderForm/OrderForm.jsx
+++ front/src/views/orderForm/OrderForm.jsx
@@ -119,8 +119,8 @@ const OrderForm = () => {
           totalPeople,
           unit_price: basePrice,
           currency_id: "ARS",
-          selectedDate: item.selectedDate,
-          selectedTime: item.selectedTime,
+          date: item.selectedDate,  
+          time: item.selectedTime, 
         
         };
       });
