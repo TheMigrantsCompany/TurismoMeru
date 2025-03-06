c578710b082779e215bcb064cfd7af197612e978
Agustina Ramos <144278185+ARagustinaramos@users.noreply.github.com>
2025-03-06 10:57:16 -0300

    Update OrderForm.jsx

--- front/src/views/orderForm/OrderForm.jsx
+++ front/src/views/orderForm/OrderForm.jsx
@@ -155,7 +155,7 @@ const OrderForm = () => {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
-              "Authorization": Bearer ${token}, 
+              "Authorization": `Bearer ${token}`,
               },
               body: JSON.stringify({
               paymentInformation: items,
