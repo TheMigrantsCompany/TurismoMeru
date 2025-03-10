6fda4c1de3ce8b4bf7a5fc4ef37abb8ef66fabe4
Agustina Ramos <144278185+ARagustinaramos@users.noreply.github.com>
2025-03-10 12:28:46 -0300

    Update createBookingHandler.js

--- back/src/handlers/booking/createBookingHandler.js
+++ back/src/handlers/booking/createBookingHandler.js
@@ -1,7 +1,7 @@
 const createBookingController = require('../../controllers/booking/createBookingController');
 
 const createBookingHandler = async (req) => {
-  const bookingData = req.body; // Extraer el body del request
+  const bookingData = data.body ? data.body : data; // Extraer el body del request
   console.log("[Handler] Datos recibidos en createBookingHandler:", bookingData);
   console.log("paymentInformation recibido:", bookingData.paymentInformation);
   
@@ -11,7 +11,8 @@ const createBookingHandler = async (req) => {
       bookingData.paymentStatus,
       bookingData.paymentInformation,
       bookingData.id_ServiceOrder,
-      bookingData.DNI
+      bookingData.DNI,
+      transaction
     );
     console.log("[Handler] Reservas creadas exitosamente:", bookings);
     return { message: 'Reserva(s) creada(s) exitosamente.', bookings };
