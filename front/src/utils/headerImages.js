a241a0abe97a3e31119332b4ddd578379da75f5b
TheMigrantsCompany <migrants.company@gmail.com>
2025-03-21 13:05:48 -0300

    Update headerImages.js

--- front/src/utils/headerImages.js
+++ front/src/utils/headerImages.js
@@ -1,8 +1,9 @@
 const headerImages = {
     
-    image1: "/src/assets/images/header/header1.jpg",
-    image2: "/src/assets/images/header/header2.jpg",
-    image3: "/src/assets/images/header/header3.jpg",
+    image1: "https://res.cloudinary.com/dzrnybyqo/image/upload/a_-90/v1742572257/playa_dnzyou.jpg",
+    image2: "https://res.cloudinary.com/dzrnybyqo/image/upload/v1742570613/_MG_9959_b4lsrk.jpg",
+    image3: "https://res.cloudinary.com/dzrnybyqo/image/upload/v1742570608/IMG_9217_mqiuvg.jpg",
+
     fondo: "/src/assets/images/fondo.jpg",
     
   };
